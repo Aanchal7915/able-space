e8b1afbe78b2f775582e2eb098121b9ba4cb4602
Aanchal7915 <Aanchal7915@users.noreply.github.com>
2026-08-21 22:53:13 +0530

    fixed

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -38,8 +38,8 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, swaggerConfig);
   SwaggerModule.setup('api/docs', app, document);
 
-  const port = process.env.PORT ?? 3001;
-  await app.listen(port);
+  const port = process.env.PORT ?? 4000;
+  await app.listen(port, '0.0.0.0');
 
   console.log(`Server listening on http://localhost:${port}/api`);
 }
